81c4885245b69d9c90acb3c83d168f3144034863
Jasmine <yx2810@columbia.edu>
2023-11-30 18:30:18 -0500

    completed search location

--- ims-front-end/src/app/search/page.js
+++ ims-front-end/src/app/search/page.js
@@ -1,9 +1,24 @@
 "use client"
 
+import { useState } from "react";
+import { useRouter, redirect } from "next/navigation";
+import { useUserContext } from "@/app/AppContext";
+
 export default function Search() {
 
-  const searchLocation = () => {
+  const [ locationId, setLocationId ] = useState();
+  const router = useRouter();
 
+  const { user } = useUserContext();
+
+  if (Object.keys(user).length === 0) {
+    redirect("/login");
+  }
+
+  const searchLocation = () => {
+    if (locationId !== null){ 
+      router.push("/location/" + locationId);
+    }
   };
 
   const searchItem = () => {
@@ -17,7 +32,7 @@ export default function Search() {
           <h2 className="text-center text-xl font-bold text-gray-900 md:text-2xl dark:text-white">Search Location</h2>
           <div>
             <label htmlFor="location-id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location ID</label>
-            <input type="text" name="location-id" id="location-id" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
+            <input onChange={e => setLocationId(e.currentTarget.value)} type="text" name="location-id" id="location-id" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="" />
           </div>
           <button onClick={searchLocation} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
         </div>
