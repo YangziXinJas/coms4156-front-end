7cd4325bfd29fa9cd16fedeff817947c50739e98
Jasmine <yx2810@columbia.edu>
2023-11-29 20:28:52 -0500

    completed order table

--- ims-front-end/src/app/client/components/OrderTable.jsx
+++ ims-front-end/src/app/client/components/OrderTable.jsx
@@ -5,17 +5,88 @@ import {
   TableHeader,
   TableColumn,
   TableBody,
+  TableRow,
+  TableCell,
 } from "@nextui-org/react"
+import { useState, useEffect, useCallback } from "react";
+import { useUserContext } from "../../AppContext";
+import Link from 'next/link'
 
 export function OrdersTable() {
+  const columns = [
+    {
+      key: "orderId",
+      label: "Order Number",
+    },
+    {
+      key: "orderDate",
+      label: "Date",
+    },
+    {
+      key: "orderStatus",
+      label: "Status"
+    }
+  ];
+  const [ data, setData ] = useState([]);
+  const { user } = useUserContext();
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8001/order/retrieve/client/" + user.clientId, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + user.token
+      },
+    });
+    
+    if (response.status === 404){
+      return response.text();
+    }
+    return response.json();
+  }
+
+  const renderCell = useCallback((order, columnKey) => {
+    const cellValue = order[columnKey];
+
+    switch (columnKey) {
+      case "orderId":
+        return (
+          <Link href={`/client/order/${cellValue}`}>{cellValue}</Link>
+        );
+      case "orderDate":
+        return (
+          <div>{cellValue}</div>
+        );
+      case "orderStatus":
+        return (
+          <div>{cellValue}</div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData().then((res) => {
+      res = res.map((item, index) => (
+        {key: index + 1 + "", orderId: item.orderId, orderDate: item.orderDate.split('T')[0], orderStatus: item.orderStatus}
+      ))
+      setData(res);
+    });
+  },[setData]);
+
+
   return (
-    <Table >
-      <TableHeader>
-        <TableColumn>NAME</TableColumn>
-        <TableColumn>ROLE</TableColumn>
-        <TableColumn>STATUS</TableColumn>
+    <Table isStriped aria-label="Example static collection table">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
-      <TableBody emptyContent={"No rows to display."}></TableBody>
+      <TableBody items={data} emptyContent={"No rows to display."}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
     </Table>
   )
 }
