5ff518428ac1d981d4699ee9e89f8c04f6087109
xyao997 <114324079+xyao997@users.noreply.github.com>
2023-12-17 21:21:35 -0500

    updated location table
    added Item name

--- ims-front-end/src/app/location/[locationId]/components/InventoryTable.jsx
+++ ims-front-end/src/app/location/[locationId]/components/InventoryTable.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Table,
@@ -7,86 +7,92 @@ import {
   TableBody,
   TableRow,
   TableCell,
-} from "@nextui-org/react"
+} from "@nextui-org/react";
 import { useState, useEffect, useCallback } from "react";
 import { useUserContext } from "@/app/AppContext";
-import Link from 'next/link'
 
 export function InventoryTable({ locationId }) {
   const columns = [
     {
       key: "itemId",
-      label: "Item",
+      label: "Item ID",
+    },
+    {
+      key: "itemName",
+      label: "Item Name",
     },
     {
       key: "quantity",
       label: "Quantity",
     },
   ];
-  const [ data, setData ] = useState([]);
+
+  const [data, setData] = useState([]);
   const { user } = useUserContext();
 
-  const fetchIventory = async () => {
-    const response = await fetch("http://localhost:8001/itemLocation/getByLocationId/" + locationId, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + user.token
-      },
-    });
-    
-    if (response.status === 404){
-      return response.text();
+  const fetchInventory = async () => {
+    try {
+      const response = await fetch(`http://localhost:8001/itemLocation/getByLocationId/${locationId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + user.token
+        },
+      });
+
+      if (!response.ok) {
+        console.error(`Error fetching inventory: ${response.statusText}`);
+        return [];
+      }
+
+      let inventoryData = await response.json();
+
+      for (let item of inventoryData) {
+        const itemResponse = await fetch(`http://localhost:8001/item/get/${item.itemId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + user.token
+          },
+        });
+        if (itemResponse.ok) {
+          const itemDetail = await itemResponse.json();
+          item.itemName = itemDetail.name;
+        } else {
+          item.itemName = 'Unknown';
+        }
+
+        item.quantity = item.quantityAtLocation;
+      }
+
+      setData(inventoryData);
+    } catch (error) {
+      console.error(`Failed to fetch inventory data: ${error}`);
     }
-    return response.json();
-  }
-
-  const renderCell = useCallback((order, columnKey) => {
-    const cellValue = order[columnKey];
-
-    switch (columnKey) {
-      default:
-        return cellValue;
-    }
-  }, []);
-
-  const objectsEqual = (o1, o2) =>
-    Object.keys(o1).length === Object.keys(o2).length 
-        && Object.keys(o1).every(p => o1[p] === o2[p]);
-
-  const arraysEqual = (a1, a2) => 
-      a1.length === a2.length && a1.every((o, idx) => objectsEqual(o, a2[idx]));
+  };
 
   useEffect(() => {
-    fetchIventory().then((res) => {
-      res = res.map((obj, index) => (
-        {key: index + 1 + "", itemId: obj.itemId, quantity: obj.quantityAtLocation}
-      ))
-      if (!arraysEqual(data, res)) {
-        setData(res);
-      };
-    });
-  },[data]);
+    fetchInventory();
+  }, [locationId]);
 
+  const renderCell = useCallback((item, columnKey) => {
+    return item[columnKey];
+  }, []);
 
   return (
-    <Table 
-      aria-label="Order History Table"
-      color={"default"}
-      defaultSelectedKeys={[]}
-      selectionMode="single"
-      fullWidth
-    >
-      <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={data} emptyContent={"There are not items in this location"}>
-        {(item) => (
-          <TableRow key={item.key}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  )
+      <Table aria-label="Inventory Table" color={"default"} selectionMode="single" fullWidth>
+        <TableHeader columns={columns}>
+          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+        </TableHeader>
+        <TableBody items={data}>
+          {(item) => (
+              <TableRow key={item.itemId}>
+                {columns.map((column) => (
+                    <TableCell key={column.key}>{renderCell(item, column.key)}</TableCell>
+                ))}
+              </TableRow>
+          )}
+        </TableBody>
+      </Table>
+  );
 };
